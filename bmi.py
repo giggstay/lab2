7d89865dcebacb1fcb77a7a36938762e739cd626
giggstay <giggstay.24@ichat.sp.edu.sg>
2025-11-05 10:20:04 +0800

    done

--- bmi.py
+++ bmi.py
@@ -4,12 +4,16 @@ def calculate_bmi(height, weight):
     bmi=weight/(height*height)
     print("BMI ="+str(bmi))
     if bmi <= 18.5:
-        print("Underweight"+" -1")
+
+        print("Underweight")
+        return -1
     
     elif bmi >=25:
-        print("Overweight"+" 1")
+        print("Overweight")
+        return 1
     
     else:
-        print("Normal Weight"+" 0")
+        print("Normal Weight")
+        return 0
     
 calculate_bmi(weight=57, height=1.73)
